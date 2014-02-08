a7fba3d247ee29c63d4c08ee2348233e79b60de6
mcflugen <mcflugen@gmail.com>
2014-02-08 10:38:31 -0700

    Added some preliminary unit tests.

--- app/wmt/tests/test_models_new.py
+++ app/wmt/tests/test_models_new.py
@@ -1,13 +1,21 @@
-from paste.fixture import TestApp
+import requests
+
 from nose.tools import *
-from wmt.scripts.wmt_wsgi_main import application
 
 
-class TestWmt():
-    def test_new(self):
-        test_app = TestApp(application)
+url_base = 'https://csdms.colorado.edu/wmt/'
 
-        resp = test_app.post('models/new/', name='test',
-                             json='{"model": 0}')
 
-        assert_equal(resp, 9)
+def test_new_model():
+    data = dict(name='Test', json='{"model": 0}')
+    resp = requests.post(url_base + 'models/new', data=data)
+
+    assert_equal(resp.status_code, 200)
+    assert_is_instance(int(resp.text), int)
+
+def test_new_model_bad_json():
+    data = dict(name='Test', json='0')
+    resp = requests.post(url_base + 'models/new', data=data)
+
+    assert_equal(resp.status_code, 200)
+    assert_is_instance(int(resp.text), int)
