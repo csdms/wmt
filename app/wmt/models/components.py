c4afae6ed738a947cf5dc22e53f7467b4ded1ac4
Eric Hutton <mcflugen@gmail.com>
2014-02-14 14:25:34 -0700

    New functions to query components.
    Added
    =====
    
    * get_component_params: Get parameters dict for a component.
    * get_component_defaults: Get default values for a component.
    * get_component_input_files: Get the template input files for a component.

--- app/wmt/models/components.py
+++ app/wmt/models/components.py
@@ -1,6 +1,7 @@
+import os
 import json
 
-from ..config import palette
+from ..config import palette, site
 
 
 class Error(Exception):
@@ -31,3 +32,51 @@ def get_component(name):
         return palette[name]
     except KeyError:
         raise IdError(name)
+
+
+def get_component_params(name):
+    comp = get_component(name)
+    return comp['parameters']
+
+
+def get_component_defaults(name):
+    defaults = {}
+
+    comp = get_component(name)
+    for parameter in comp['parameters']:
+        defaults[parameter['key']] = parameter['value']['default']
+
+    #return '{january_temperature_mean}'.format(**defaults)
+    #return str(type(defaults['january_temperature_mean']))
+    return defaults
+
+
+def get_component_input_files(name, with_defaults=False):
+    import string
+
+    input_file_dir = os.path.join(site['data'], 'components')
+
+    comp = get_component(name)
+
+    if 'input_files' not in comp:
+        return ''
+
+    mapping = get_component_defaults(name)
+
+    lines = []
+    for filename in comp['input_files']:
+        path_to_file = os.path.join(input_file_dir, filename)
+        lines.append('file: %s' % filename)
+        lines.append('-------------------')
+        with open(path_to_file, 'r') as file:
+            if with_defaults:
+                #contents = string.Template(file.read())
+                contents = file.read()
+                lines.append(contents.format(**mapping))
+                #lines.append(contents.substitute(mapping))
+            else:
+                lines.append(file.read())
+        lines.append('-------------------')
+
+    return os.linesep.join(lines)
+
