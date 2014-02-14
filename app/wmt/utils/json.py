4160e0a7383c472700dd984dd7be9ed251deb2b7
Eric Hutton <mcflugen@gmail.com>
2014-02-14 14:30:18 -0700

    Added new functions moved from cca.json
    Added
    =====
    
    * The folowing new functions were moved from cca.json:
      - load_component
      - load_component_defaults

--- app/wmt/utils/json.py
+++ app/wmt/utils/json.py
@@ -1,7 +1,9 @@
+from __future__ import absolute_import
+
+import json
+
 
 def load_mapping(filename):
-    import json
-
     with open(filename, 'r') as map:
         mapping = json.loads(map.read())
 
@@ -9,3 +11,27 @@ def load_mapping(filename):
         raise ValueError(filename)
 
     return mapping
+
+
+def load_component(filename):
+    with open(filename, 'r') as file:
+        contents = file.read()
+
+    try:
+        desc = json.loads(contents)
+    except ValueError:
+        raise
+    else:
+        if desc.has_key('parameters'):
+            return desc
+        else:
+            raise ValueError(filename)
+
+
+def load_component_defaults(filename):
+    defaults = {}
+    desc = load_component(filename)
+    for parameter in desc['parameters']:
+        defaults[parameter['key']] = parameter['value']['default']
+
+    return defaults
