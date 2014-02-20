ae62f2855b2b6ab3154df2d14769b29aa426359c
Eric Hutton <mcflugen@gmail.com>
2014-02-20 11:47:51 -0700

    Added new component description JSON files.

--- app/wmt/utils/db.py
+++ app/wmt/utils/db.py
@@ -1,19 +1,64 @@
+from __future__ import print_function
+
 import os
+from sys import stderr as STDERR
 
 from .json import load_component
+
+
+class Error(Exception):
+    pass
+
+
+class LoadError(Error):
+    def __init__(self, msg):
+        self._msg = msg
+
+    def __str__(self):
+        return self._msg
+
+
+def _component_names(palette_dir):
+    names = set()
+    for item in os.listdir(palette_dir):
+        if os.path.isdir(os.path.join(palette_dir, item)):
+            names.add(item)
+    names = ['hydrotrend']
+    return names
+
+
+def _load_table(name, db='.'):
+    path_to_table = os.path.join(db, name + '.db')
+    try:
+        with open(path_to_table, 'r') as opened:
+            try:
+                table = json.loads(opened.read())
+            except ValueError as error:
+                raise LoadError(str(error))
+    except IOError as error:
+        raise LoadError(str(error))
+
+    return table
+
+
+def _construct_component_from_db(path_to_db):
+    desc = _load_table('info', db=path_to_db)
+
+    for table in ['parameters', 'uses', 'provides']:
+        desc[table] = _load_table(table, db=path_to_db)
+
+    return desc
 
 
 def load_palette(palette_dir):
 
     palette = {}
-    for item in os.listdir(palette_dir):
-        if os.path.isdir(os.path.join(palette_dir, item)):
-            path_to_json = os.path.join(palette_dir, item, item + '.json')
-            try:
-                desc = load_component(path_to_json)
-            except ValueError:
-                logger.error('%s: unable to load component' % path_to_json)
-            else:
-                palette[desc['id']] = desc
+    for name in _component_names(palette_dir):
+        path_to_db = os.path.join(palette_dir, name, 'db')
+        try:
+            palette[name] = _construct_component_from_db(path_to_db)
+        except LoadError:
+            raise
+            print('%s: unable to load' % name, file=STDERR)
 
     return palette
