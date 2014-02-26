3a5cac127b16f2fe0d2ce256bb5a31c5cc71985d
Eric Hutton <mcflugen@gmail.com>
2014-02-26 11:24:08 -0700

    Added download_file function that downloads a run stage file.

--- app/wmt/utils/io.py
+++ app/wmt/utils/io.py
@@ -21,3 +21,22 @@ def chunk_copy(src_fp, dest_fp, chunk_size=8192, checksum=None):
 def upload_large_file(filename, url):
     with open(filename, 'r') as fp:
         return requests.post(url, data=fp)
+
+
+def download_file(url):
+    resp = requests.get(url, stream=True)
+
+    try:
+        for attr in resp.headers['content-disposition'].split(';'):
+            if attr.strip().startswith('filename'):
+                dest_name = attr[attr.index('=') + 1:].strip()
+    except KeyError:
+        dest_name = os.path.basename(url)
+
+    with open(dest_name, 'wb') as fp:
+        for chunk in resp.iter_content():
+            if chunk: # filter out keep-alive new chunks
+                fp.write(chunk)
+                fp.flush()
+
+    return dest_name
