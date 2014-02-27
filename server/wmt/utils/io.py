09d9574050521efe8e85f907440855b31272c2ef
Eric Hutton <mcflugen@gmail.com>
2014-02-27 16:10:34 -0700

    Changed chunk_copy so that it creates the checksum and returns it.

--- server/wmt/utils/io.py
+++ server/wmt/utils/io.py
@@ -1,21 +1,29 @@
+import os
 import requests
 
 
 def _copy_a_chunk(src_fp, dest_fp, chunk_size, checksum=None):
     chunk = src_fp.read(chunk_size)
-    if not chunk:
+    if not chunk or len(chunk) == 0:
         raise EOFError
     dest_fp.write(chunk)
-    if checksum is not None:
+    try:
         checksum.update(chunk)
+    except AttributeError:
+        pass
 
 
-def chunk_copy(src_fp, dest_fp, chunk_size=8192, checksum=None):
+def chunk_copy(src_fp, dest_fp, chunk_size=8192):
+    import hashlib
+    checksum = hashlib.md5()
+
     while 1:
         try:
             _copy_a_chunk(src_fp, dest_fp, chunk_size, checksum=checksum)
         except EOFError:
             break
+
+    return checksum
 
 
 def upload_large_file(filename, url):
