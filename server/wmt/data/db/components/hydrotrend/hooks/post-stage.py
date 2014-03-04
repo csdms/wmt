883bcdf95507c25cda47579c311e678cf484b3d4
Eric Hutton <mcflugen@gmail.com>
2014-03-04 12:08:27 -0700

    Changed to look for .hyps files in staging dir and database.

--- server/wmt/data/db/components/hydrotrend/hooks/post-stage.py
+++ server/wmt/data/db/components/hydrotrend/hooks/post-stage.py
@@ -2,6 +2,14 @@ import os
 import shutil
 
 
+def find_file(filename, paths=['.', ]):
+    for path in paths:
+        path_to_file = os.path.join(path, filename)
+        if os.path.isfile(path_to_file):
+            return path_to_file
+    return None
+
+
 def execute(env):
     this_dir = os.path.abspath(os.path.dirname(__file__))
 
@@ -11,6 +19,9 @@ def execute(env):
     os.rename('hydrotrend.in', 'HYDRO.IN')
     shutil.move('HYDRO.IN', 'HYDRO_IN')
 
-    shutil.copy(
-        os.path.join(this_dir, '..', 'files', env['hypsometry_file']),
-        os.path.join('HYDRO_IN', 'HYDRO0.HYPS'))
+    src = find_file(env['hypsometry_file'])
+    if src is not None:
+        shutil.copy(src, os.path.join('HYDRO_IN', 'HYDRO0.HYPS'))
+    else:
+        raise ValueError(env['hypsometry_file'])
+
