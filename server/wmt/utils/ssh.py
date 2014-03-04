a16c93ae6c23a9571bda198c82fa454734f76ffa
Eric Hutton <mcflugen@gmail.com>
2014-03-04 10:49:55 -0700

    Added new function launch_cmt_on_host.
    Added
    =====
    
    * launch_cmt_on_host function sets up a simulation on a remote server
      through ssh and then runs the simulation.

--- server/wmt/utils/ssh.py
+++ server/wmt/utils/ssh.py
@@ -2,6 +2,68 @@ import paramiko
 import getpass
 import os
 
+from ..config import site
+
+
+def open_connection_to_host(host, username, password=None):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(host, username=username, password=password)
+    except paramiko.AuthenticationException:
+        ssh.connect(host, username=username, password=getpass.getpass())
+
+    sftp = ssh.open_sftp()
+
+    return (ssh, sftp)
+
+
+def make_wmt_prefix(ssh, remote_path):
+    ssh.exec_command(' '.join(['mkdir', '-p', remote_path]))
+
+
+def copy_launch_file(sftp, local_path, remote_path):
+    filename = os.path.basename(local_path)
+    try:
+        sftp.put(local_path, os.path.join(remote_path, filename))
+    except IOError as error:
+        raise ValueError(' -> '.join([local_path, os.path.join(remote_path, filename)]))
+
+
+def execute_launch_command(ssh, remote_path, uuid):
+    cd_to_path = 'cd %s' % remote_path
+    run_script = '/usr/bin/env -i /bin/bash ./launch.sh %s' % uuid
+
+    stdin, stdout, stderr = ssh.exec_command(cd_to_path + ' && ' + run_script + ' &')
+
+    return (stdin, stdout, stderr)
+
+
+def get_host_launch_files(host):
+    return [
+        os.path.join(site['db'], 'hosts', host, 'files', 'launch.sh'),
+        os.path.join(os.path.dirname(__file__), '..', 'scripts', 'launch.py'),
+    ]
+
+
+def launch_cmt_on_host(uuid, host, username, password=None, args=[]):
+    (ssh, sftp) = open_connection_to_host(host, username, password=password)
+
+    remote_path = os.path.join('.wmt', uuid)
+
+    make_wmt_prefix(ssh, remote_path)
+
+    for file in get_host_launch_files(host):
+        copy_launch_file(sftp, file, remote_path)
+
+    (_, stdout, stderr) = execute_launch_command(ssh, remote_path, uuid)
+
+    return {
+        'stdout': ''.join(stdout.readlines()),
+        'stderr': ''.join(stderr.readlines()),
+    }
+
 
 def launch_command_on_host(username, host, script, password='', args=[]):
     ssh = paramiko.SSHClient()
@@ -16,7 +78,9 @@ def launch_command_on_host(username, host, script, password='', args=[]):
         return 'Ssh error: ssh %s@%s:%s' % (username, host, password)
 
     sftp = ssh.open_sftp()
-    sftp.put(script, os.path.basename(script))
+
+    pre_launch(ssh, sftp)
+
 
     stdin, stdout, stderr = ssh.exec_command(
         ' '.join(
