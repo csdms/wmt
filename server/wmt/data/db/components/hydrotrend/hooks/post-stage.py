5e3843cbb8728430699bcfaef97d83a3a5d7f0d9
Eric Hutton <mcflugen@gmail.com>
2014-03-10 13:47:24 -0600

    Changed hydrotrend post-stage hook to use new find_simulation_file function.

--- server/wmt/data/db/components/hydrotrend/hooks/post-stage.py
+++ server/wmt/data/db/components/hydrotrend/hooks/post-stage.py
@@ -1,25 +1,23 @@
 import os
 import shutil
 
-
-def find_file(filename, paths=['.', ]):
-    for path in paths:
-        path_to_file = os.path.join(path, filename)
-        if os.path.isfile(path_to_file):
-            return path_to_file
-    return None
+from wmt.utils.hook import find_file
 
 
 def execute(env):
-    this_dir = os.path.abspath(os.path.dirname(__file__))
-
     os.mkdir('HYDRO_IN')
     os.mkdir('HYDRO_OUTPUT')
 
     os.rename('hydrotrend.in', 'HYDRO.IN')
     shutil.move('HYDRO.IN', 'HYDRO_IN')
 
-    src = find_file(env['hypsometry_file'])
+    try:
+        src = find_simulation_file(env['hypsometry_file'])
+    except FileNotFoundError:
+        raise ValueError(env['hypsometry_file'])
+
+    shutil.copy(src, os.path.join('HYDRO_IN', 'HYDRO0.HYPS'))
+
     if src is not None:
         shutil.copy(src, os.path.join('HYDRO_IN', 'HYDRO0.HYPS'))
     else:
