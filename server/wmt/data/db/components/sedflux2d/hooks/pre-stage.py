f2ff1a14a5debe408e641b54f8deebed62b97999
Eric Hutton <mcflugen@gmail.com>
2014-03-11 15:26:51 -0600

    Added code that calculates derived parameters for bathymetry file.

--- server/wmt/data/db/components/sedflux2d/hooks/pre-stage.py
+++ server/wmt/data/db/components/sedflux2d/hooks/pre-stage.py
@@ -1,5 +1,17 @@
 
 
 def execute(env):
+    for name in ['run_duration', 'shelf_gradient', 'shelf_width',
+                 'slope_gradient', 'domain_width']:
+        env[name] = float(env[name])
+
     env['sea_level_start_time'] = 0.
-    env['sea_level_end_time'] = float(env['run_duration']) / 365.
+    env['sea_level_end_time'] = env['run_duration'] / 365.
+
+    env['shelf_slope_break_elevation'] = - (env['shelf_gradient'] *
+                                            env['shelf_width'])
+    env['end_of_domain_elevation'] = (
+        env['shelf_slope_break_elevation'] -
+        env['slope_gradient'] * (env['domain_width'] - env['shelf_width'])
+    )
+
