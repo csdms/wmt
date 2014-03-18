177667cdae509c9d5de0205fe4001f6e55458ce3
Eric Hutton <mcflugen@gmail.com>
2014-03-18 17:26:06 -0600

    Changed templates to use a base for css and global variables like the base url.

--- server/wmt/render.py
+++ server/wmt/render.py
@@ -1,6 +1,14 @@
 import web
+import urlparse
 
 from .config import site
 
+parts = urlparse.urlsplit('https://csdms.colorado.edu/wmt-server')
+globals = {
+    'SCHEME': parts.scheme,
+    'HOST': parts.netloc,
+    'PREFIX': parts.path,
+    'BASE_URL': urlparse.urlunsplit(parts),
+}
 
-render = web.template.render(site['templates'])
+render = web.template.render(site['templates'], base='base', globals=globals)
