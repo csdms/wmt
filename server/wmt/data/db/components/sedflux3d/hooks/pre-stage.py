835809d4a9a7e04a90adb7c60333f3a233cbcf58
Eric Hutton <mcflugen@gmail.com>
2014-03-20 15:43:48 -0600

    Removed component metadata database from repository; it's now in its own repo.

--- server/wmt/data/db/components/sedflux3d/hooks/pre-stage.py
+++
@@ -1,8 +0,0 @@
-
-
-def execute(env):
-    for name in ['run_duration', ]:
-        env[name] = float(env[name])
-
-    env['sea_level_start_time'] = 0.
-    env['sea_level_end_time'] = env['run_duration'] / 365.
