1121c4ccdf336d7444fb07ad18c47b43b7a8f9a5
Mark Piper <mark.piper@colorado.edu>
2014-04-18 17:20:19 -0600

    Add "Manage labels" button to MoreActionsMenu

--- gui/src/edu/colorado/csdms/wmt/client/ui/MoreActionsMenu.java
+++ gui/src/edu/colorado/csdms/wmt/client/ui/MoreActionsMenu.java
@@ -6,6 +6,8 @@ package edu.colorado.csdms.wmt.client.ui;
 import java.util.Iterator;
 
 import com.google.gwt.dom.client.Style.Cursor;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -27,6 +29,7 @@ public class MoreActionsMenu extends PopupPanel {
 
   @SuppressWarnings("unused")
   private DataManager data;
+  private LabelsMenu labelsMenu;
   
   /**
    * Makes a new {@link MoreActionsMenu}.
@@ -44,13 +47,33 @@ public class MoreActionsMenu extends PopupPanel {
     VerticalPanel menu = new VerticalPanel();
     this.add(menu);
 
-    // Save As
+    // Save as
     HTML saveAsButton =
         new HTML(DataManager.FA_SAVE + "Save model as...");
-    saveAsButton.setTitle("Save model as...");
+    saveAsButton.setTitle("Save model with a new name");
     saveAsButton.addClickHandler(new ModelActionPanelSaveHandler(data, true));    
     menu.add(saveAsButton);
 
+    // Manage labels
+    final HTML labelsButton =
+        new HTML(DataManager.FA_TAGS + "Manage labels...");
+    labelsButton.setTitle("Manage labels set on models");
+    menu.add(labelsButton);
+    labelsMenu = new LabelsMenu(data);
+    labelsButton.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        labelsMenu.setPopupPositionAndShow(new PositionCallback() {
+          final Integer x = labelsButton.getElement().getAbsoluteRight();
+          final Integer y = labelsButton.getAbsoluteTop();
+          @Override
+          public void setPosition(int offsetWidth, int offsetHeight) {
+            labelsMenu.setPopupPosition(x, y);
+          }
+        });
+      }
+    });
+    
     // Delete
     HTML deleteButton = new HTML(DataManager.FA_DELETE + "Delete model...");
     deleteButton.setTitle("Delete model");
