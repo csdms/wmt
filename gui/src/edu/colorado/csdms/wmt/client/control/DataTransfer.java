28c364c3c964e470c5f6144eaf48d08cca3beaa1
Mark Piper <mark.piper@colorado.edu>
2014-04-21 13:28:50 -0600

    On login, set username as a model label
    On logout, remove this label. (Alternately, uncheck this label. Need to
    think about this.)

--- gui/src/edu/colorado/csdms/wmt/client/control/DataTransfer.java
+++ gui/src/edu/colorado/csdms/wmt/client/control/DataTransfer.java
@@ -536,6 +536,7 @@ public class DataTransfer {
               + "</b> | <i class='fa fa-sign-out'></i> "
               + "<a href=\"javascript:;\">Logout</a>");
       data.getPerspective().getLoginHtml().setTitle("Logout from WMT");
+      data.modelLabels.put(data.security.getWmtUsername(), true);
     }
 
     /*
@@ -547,6 +548,7 @@ public class DataTransfer {
           "<i class='fa fa-sign-in'></i> "
               + "<a href=\"javascript:;\">Login</a>");
       data.getPerspective().getLoginHtml().setTitle("Login to WMT");
+      data.modelLabels.remove(data.security.getWmtUsername());
     }
     
     @Override
