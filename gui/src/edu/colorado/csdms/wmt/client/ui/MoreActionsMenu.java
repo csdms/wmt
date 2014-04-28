a7cd6545588bcb0c9432b97795d9a39d75c0cded
Mark Piper <mark.piper@colorado.edu>
2014-04-28 15:03:03 -0600

    Use CSS to set pointer type in MoreActionsMenu

--- gui/src/edu/colorado/csdms/wmt/client/ui/MoreActionsMenu.java
+++ gui/src/edu/colorado/csdms/wmt/client/ui/MoreActionsMenu.java
@@ -5,7 +5,6 @@ package edu.colorado.csdms.wmt.client.ui;
 
 import java.util.Iterator;
 
-import com.google.gwt.dom.client.Style.Cursor;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.HTML;
@@ -41,7 +40,6 @@ public class MoreActionsMenu extends PopupPanel {
   public MoreActionsMenu(DataManager data) {
 
     super(true); // autohide
-    this.getElement().getStyle().setCursor(Cursor.POINTER); // use pointer
     this.data = data;
     this.setStyleName("wmt-PopupPanel");
 
