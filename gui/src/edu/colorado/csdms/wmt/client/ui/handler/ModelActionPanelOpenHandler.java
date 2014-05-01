0429e271eef3834de492277ba8c471a2508d7a9e
Mark Piper <mark.piper@colorado.edu>
2014-05-01 15:05:19 -0600

    Add "Enter" and "Esc" shortcuts to model open dialog
    Hitting “Enter” key fires event for “OK” button on dialog. Hitting
    “Esc” key fires event for “Cancel” button.

--- gui/src/edu/colorado/csdms/wmt/client/ui/handler/ModelActionPanelOpenHandler.java
+++ gui/src/edu/colorado/csdms/wmt/client/ui/handler/ModelActionPanelOpenHandler.java
@@ -5,14 +5,19 @@ package edu.colorado.csdms.wmt.client.ui.handler;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
 
 import edu.colorado.csdms.wmt.client.control.DataManager;
 import edu.colorado.csdms.wmt.client.ui.widgets.OpenDialogBox;
 
 /**
- * Handles click on the "Open Model..." button in the ModelActionPanel. Pops up an
- * instance of {@link OpenDialogBox} to prompt the user for a model to open.
- * Events are sent to {@link OpenModelHandler} and {@link DialogCancelHandler}.
+ * Handles click on the "Open Model..." button in the ModelActionPanel. Pops up
+ * an instance of {@link OpenDialogBox} to prompt the user for a model to open.
+ * Events are sent to {@link OpenModelHandler} (on clicking "OK" button or
+ * hitting <code>Enter</code> key) and {@link DialogCancelHandler} (on clicking
+ * "Cancel" or hitting <code>Esc</code> key).
  */
 public class ModelActionPanelOpenHandler implements ClickHandler {
 
@@ -39,14 +44,33 @@ public class ModelActionPanelOpenHandler implements ClickHandler {
           data.modelNameList.get(i));
     }
 
-    openDialog.getChoicePanel().getOkButton().addClickHandler(
-        new OpenModelHandler(data, openDialog));
-    openDialog.getChoicePanel().getCancelButton().addClickHandler(
-        new DialogCancelHandler(openDialog));
+    // Define open and cancel handlers.
+    final OpenModelHandler openHandler = new OpenModelHandler(data, openDialog);
+    final DialogCancelHandler cancelHandler =
+        new DialogCancelHandler(openDialog);
+
+    // Set the open and cancel handlers on the "OK" and "Cancel" buttons.
+    openDialog.getChoicePanel().getOkButton().addClickHandler(openHandler);
+    openDialog.getChoicePanel().getCancelButton()
+        .addClickHandler(cancelHandler);
+
+    // Also use the handlers for the "Enter" and "Esc" keys.
+    // XXX There's likely a more stylish way to do this.
+    openDialog.addDomHandler(new KeyDownHandler() {
+      @Override
+      public void onKeyDown(KeyDownEvent event) {
+        Integer keyCode = event.getNativeKeyCode();
+        if (keyCode == KeyCodes.KEY_ESCAPE) {
+          cancelHandler.onClick(null);
+        } else if (keyCode == KeyCodes.KEY_ENTER) {
+          openHandler.onClick(null);
+        }
+      }
+    }, KeyDownEvent.getType());
 
     openDialog.center();
 
     // Give the droplist focus.
-    openDialog.getDroplistPanel().getDroplist().setFocus(true);    
+    openDialog.getDroplistPanel().getDroplist().setFocus(true);
   }
 }
