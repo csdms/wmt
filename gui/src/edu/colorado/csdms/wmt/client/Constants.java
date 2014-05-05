5c9819fa36a69b40fb22f5a5446325db149e8283
Mark Piper <mark.piper@colorado.edu>
2014-05-05 11:52:38 -0600

    Create NewUserDialogBox
    This dialog box is shown if an unrecognized email address is used to
    sign in to WMT. The user is given a choice of reentering their password
    and using the email address as their sign in, or canceling.

--- gui/src/edu/colorado/csdms/wmt/client/Constants.java
+++ gui/src/edu/colorado/csdms/wmt/client/Constants.java
@@ -104,6 +104,7 @@ public class Constants {
   public static String FA_MORE = " <i class='fa fa-caret-down'></i>";
   public static String FA_COG = "<i class='fa fa-cog fa-fw'></i> ";
   public static String FA_COGS = "<i class='fa fa-cogs fa-fw'></i> ";
+  public static String FA_USER = "<i class='fa fa-user fa-fw'></i> ";
 
   // Tooltip text strings.
   public static String MODEL_OPEN = "Open an existing model.";
