204c1594e2994d5da41fc1b7d97c97333eef2ac1
Mark Piper <mark.piper@colorado.edu>
2014-05-08 10:25:24 -0600

    Add test for new methods in PortJSO

--- gui/test/edu/colorado/csdms/wmt/client/PortJSOTest.java
+++ gui/test/edu/colorado/csdms/wmt/client/PortJSOTest.java
@@ -1,32 +1,56 @@
+/**
+ * <License>
+ */
 package edu.colorado.csdms.wmt.client;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.junit.client.GWTTestCase;
 
 import edu.colorado.csdms.wmt.client.data.PortJSO;
 
 /**
- * Tests for {@link PortJSO}.
+ * Tests for {@link PortJSO}. JUnit integration is provided by extending
+ * {@link GWTTestCase}.
  * 
+ * @see http://www.gwtproject.org/doc/latest/DevGuideTesting.html
+ * @see http://www.gwtproject.org/doc/latest/tutorial/JUnit.html
+ * @see http://blog.danielwellman.com/2008/08/testing-json-parsing-using-javascript-overlay-types-in-gwt-15.html
  * @author Mark Piper (mark.piper@colorado.edu)
  */
 public class PortJSOTest extends GWTTestCase {
 
   private PortJSO portJSO;
   private String id;
-  private Boolean required;
+  private boolean required;
+  private JsArrayString exchange_items;
   
+  /**
+   * The module that sources this class. Must be present.
+   */
   @Override
   public String getModuleName() {
      return "edu.colorado.csdms.wmt.WMT";
   }
   
-  // A JSNI method that defines a fixture for the tests.
-  private native PortJSO testPortJSO(String id, boolean required) /*-{ 
-    return {"required":required, "id":id} 
+  /**
+   * A JSNI method that defines a fixture for the tests. Returns a PortJSO
+   * object for testing.
+   * 
+   * @param id
+   * @param required
+   * @param exchange_items
+   */
+  private native PortJSO testPortJSO(String id, boolean required,
+      JsArrayString exchange_items) /*-{
+		return {
+			"required" : required,
+			"id" : id,
+			"exchange_items" : exchange_items
+		}
   }-*/;  
   
   @Before
@@ -34,7 +58,11 @@ public class PortJSOTest extends GWTTestCase {
   protected void gwtSetUp() throws Exception {
     id = "test";
     required = true;
-    portJSO = testPortJSO(id, required);
+    exchange_items = (JsArrayString) JsArrayString.createArray();
+    exchange_items.push("foo");
+    exchange_items.push("bar");
+    exchange_items.push("baz");
+    portJSO = testPortJSO(id, required, exchange_items);
   }
 
   @After
@@ -61,8 +89,13 @@ public class PortJSOTest extends GWTTestCase {
 
   @Test
   public void testSetIsRequired() {
-    Boolean isRequired = false;
+    boolean isRequired = false;
     portJSO.isRequired(isRequired);
-//    assertFalse(portJSO.isRequired());
+    assertFalse(portJSO.isRequired());
+  }
+
+  @Test
+  public void testGetExchangeItems() {
+    assertEquals(exchange_items, portJSO.getExchangeItems());
   }
 }
