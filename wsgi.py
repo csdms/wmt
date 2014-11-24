72802e6b854def401fc0e231fc4b445f13dacb39
mcflugen <mcflugen@gmail.com>
2014-11-24 15:52:28 -0700

    Use the absolute path to wsgi script.

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ from wmt.flask import create_app
 if __name__ == "__main__":
     from os import path
 
-    create_app(wmt_root_path=path.dirname(__file__)).run()
+    create_app(wmt_root_path=path.abspath(path.dirname(__file__))).run()
