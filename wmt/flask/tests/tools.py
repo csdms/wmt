1f3326f4f8ca931053e4fc717aa98b5647638e18
mcflugen <mcflugen@gmail.com>
2014-11-25 17:27:22 -0700

    Added assertions for resources and collections.
    Added
    -----
    * New validator classes to validate collections and resources. These
      classes are meant to be subclassed for specific resources.

--- wmt/flask/tests/tools.py
+++ wmt/flask/tests/tools.py
@@ -1,6 +1,70 @@
 import json
-from nose.tools import assert_equal, assert_true
+from nose.tools import (assert_equal, assert_true, assert_is_instance,
+                        assert_less_equal)
 from . import FAKE_USER_NAME, FAKE_USER_PASS
+
+
+class ResourceValidator(object):
+    __meta_fields__ = set()
+    __type__ = None
+    __fields__ = set()
+
+    def __init__(self, type=None, fields=None):
+        self._fields = set(fields or self.__fields__)
+        self._type = type or self.__type__
+
+    def __call__(self, resource, **kwds):
+        assert_is_instance(resource, dict)
+
+        actual = set(resource.keys())
+        expected = set(self._fields) | set(self.__meta_fields__)
+
+        assert_equal(expected, actual)
+        assert_less_equal(set(kwds.keys()), expected)
+
+        for k, v in kwds.items():
+            assert_equal(resource[k], v)
+
+
+class CollectionValidator(object):
+    __validator__ = ResourceValidator()
+
+    def __init__(self, validator=None):
+        self._validator = validator or self.__validator__
+
+    def __call__(self, collection, **kwds):
+        assert_is_instance(collection, list)
+        if self.__validator__:
+            for item in collection:
+                self.__validator__(item, **kwds)
+
+
+class AssertIsResourceResponse(ResourceValidator):
+    __meta_fields__ = ['@type']
+
+    def __call__(self, resp, **kwds):
+        if hasattr(resp, 'data'):
+            try:
+                resource = json.loads(resp.data)
+            except ValueError:
+                raise AssertionError('not a JSON object')
+        else:
+            resource = resp
+        super(AssertIsResourceResponse, self).__call__(resource, **kwds)
+
+
+class AssertIsCollectionResponse(CollectionValidator):
+    def __call__(self, resp, **kwds):
+        if hasattr(resp, 'data'):
+            try:
+                collection = json.loads(resp.data)
+            except ValueError:
+                raise AssertionError('not a JSON object')
+        super(AssertIsCollectionResponse, self).__call__(collection, **kwds)
+
+
+assert_is_resource = AssertIsResourceResponse()
+assert_is_collection = AssertIsCollectionResponse()
 
 
 def assert_401_unauthorized(resp):
