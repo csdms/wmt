1bb7a28f3bfa64c3f13005d40711cb05852b30a9
mcflugen <mcflugen@gmail.com>
2015-06-03 17:27:47 -0600

    Updated requirements and install_requires.

--- setup.py
+++ setup.py
@@ -12,11 +12,14 @@ from wmt import __version__
 
 setup(name='wmt',
       version=__version__,
-      description='Web app for the Component Modeling Tool',
+      description='RESTful API for the Web Modeling Tool',
       author='Eric Hutton',
       author_email='eric.hutton@colorado.edu',
       url=' http://csdms.colorado.edu/',
-      install_requires=['web.py', 'PyYAML>=3.10', 'passlib', ],
+      install_requires=['flask', 'flask-sqlalchemy', 'flask-testing',
+                        'flask-login', 'sqlalchemy-migrate', 'PyYAML>=3.10',
+                        'passlib', 'sphinxcontrib-httpdomain==1.1.8',
+                        'cmtstandardnames', 'paramiko']
       packages=find_packages(),
       #packages=['wmt', 'wmt.scripts', 'wmt.models', ],
       long_description="Create, save, edit, run collections of connected components.",
