c37947bc204a6e2332d80268c1182f85b2e333ae
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 08:14:59 +0000

    Adding tarfile member sanitization to extractall()

--- wmt/scripts/wmt-slave.py
+++ wmt/scripts/wmt-slave.py
@@ -219,7 +219,26 @@ class WmtSlave(object):
 
     def unpack_tarball(self, path):
         with tarfile.open(path) as tar:
-            tar.extractall(path=self._wmt_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=self._wmt_dir)
 
     def pack_tarball(self):
         os.chdir(self._wmt_dir)
